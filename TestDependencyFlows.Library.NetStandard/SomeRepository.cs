7471f01144852ff0ad378f102e2c03d534de82bc
James Dunlop <jamesd@campaignmonitor.com>
2019-06-21 16:07:34 +1000

    Working solution

---
+++ TestDependencyFlows.Library.NetStandard/SomeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestDependencyFlows.Library.NetStandard
+{
+    public static class SomeRepository
+    {
+        public static List<string> GetSettings()
+        {
+            // Some code that can't be optimised away
+            Action<Type> noop = _ => { };
+            var typeHook = typeof(System.Data.SqlClient.SqlClientFactory);
+            noop(typeHook);
+
+            return new List<string> { "Found data !" };
+        }
+    }
+}
---
+++ TestDependencyFlows.Library/SomeService.cs
@@ -0,0 +1,12 @@
+﻿using TestDependencyFlows.Library.NetStandard;
+
+namespace TestDependencyFlows.Library
+{
+    public static class SomeService
+    {
+        public static string GetSomething()
+        {
+            return SomeRepository.GetSettings()[0];
+        }
+    }
+}
